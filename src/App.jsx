580276e4effb9b045508eff8a450db9e6db7e209
Mario Rico <mario.rico417@gmail.com>
2024-04-15 22:14:05 -0500

    retrying commit because of an issue with vscode

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import {Routes, Route, Link} from "react-router-dom";
+import "./App.css";
+import { Blue } from "./components/Blue";
+import { Red } from "./components/Red";
+import { Home } from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-
-    </>
-  )
+	return (
+		<>
+			<div id="container">
+				<h1>Hello React Router!</h1>
+				<div id="navbar">
+          <Link to="/">Home</Link>  
+          <Link to="/blue">Blue</Link>  
+          <Link to="/Red">Red</Link>  
+        </div>
+				<div id="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+        </div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
