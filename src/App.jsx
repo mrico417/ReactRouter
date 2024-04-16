12e2ac8a724cb3c5f9bb6a14eb33b8391cd41f36
Mario Rico <mario.rico417@gmail.com>
2024-04-16 11:40:16 -0500

    created Navbar.jsx and Mainsection.jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-import {Routes, Route, Link} from "react-router-dom";
 import "./App.css";
-import { Blue } from "./components/Blue";
-import { Red } from "./components/Red";
-import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { Mainsection } from "./components/Mainsection";
 
 function App() {
 	return (
 		<>
 			<div id="container">
-				<h1>Hello React Router!</h1>
-				<div id="navbar">
-          <Link to="/">Home</Link>  
-          <Link to="/blue">Blue</Link>  
-          <Link to="/Red">Red</Link>  
-        </div>
-				<div id="main-section">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} />
-          </Routes>
-        </div>
+				<Navbar />
+				<Mainsection />
 			</div>
 		</>
 	);
